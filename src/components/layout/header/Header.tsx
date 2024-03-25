acd4f9ca98dc7855a567d59cb289bdb58286ea80
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-25 20:58:07 +0500

    little changes and styling in header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -4,7 +4,8 @@ import classes from "./style.module.css";
 const Header = () => {
     return (
             <header className={classes.header}>
-
+                <img className={classes.header__logo} src="" alt=""/>
+                <div></div>
             </header>
     );
 };
