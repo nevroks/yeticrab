cbecd09f7b36ba019add03002eff3f8f8fcf3381
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-25 20:58:56 +0500

    removed unused pages from routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import {Route, Routes} from "react-router-dom";
 import React from "react";
 import Layout from "./components/layout/Layout.tsx";
 import HomePage from "./pages/homepage/HomePage.tsx";
-import TodosPage from "./pages/todospage/TodosPage.tsx";
-import ProfilePage from "./pages/profilepage/ProfilePage.tsx";
-import LoginPage from "./pages/loginpage/LoginPage.tsx";
 
 
 function App() {
@@ -12,11 +9,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path={"/login"} element={<LoginPage/>}/>
         <Route path="/" element={<Layout/>}>
           <Route index element={<HomePage/>}/>
-          <Route path="todos" element={<TodosPage/>}/>
-          <Route path="profile" element={<ProfilePage/>}/>
         </Route>
       </Routes>
     </>
