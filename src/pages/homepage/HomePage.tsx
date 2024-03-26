ffb5fd563c719f0da812eb49858d64ef1315d66e
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-27 00:59:46 +0500

    added home page and styled it

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,56 +1,13 @@
-import React, {useEffect, useMemo} from 'react';
+import React from 'react';
+import logo from "./../../assets/logo.png"
 import classes from "./style.module.css";
-import {useAppDispatch, useAppSelector} from "../../hooks/reduxHooks.tsx";
 import {Button} from "@gravity-ui/uikit";
-import {toggleAdmin} from "../../store/admin/adminSlice.ts";
-import Apply from "../../components/applies/Apply.tsx";
-import {IApply} from "../../types/types.tsx";
-import {addApply} from "../../store/applies/appliesSlice.ts";
-import AppliesList from "../../components/applies/AppliesList.tsx";
-
+import {Link} from "react-router-dom";
 const HomePage = () => {
-    const isAdmin=useAppSelector(state => state.admin.value)
-    const countOfApplies=useAppSelector(state => state.applies.length)
-    const dispatch=useAppDispatch()
-    const newApply:IApply=useMemo(()=>{
-        return {
-            id:countOfApplies+1,
-            date:'',
-            company:"Название компании клиента",
-            deliver:"ФИО перевозчика",
-            phoneNumber:'Номер перевозчика',
-            commentary:'Коментарий',
-            status:'Новая',
-            ati:`https://ati.su/firms/${countOfApplies+1}/info`
-        }
-    },[countOfApplies])
-    useEffect(()=>{
-        if (localStorage.getItem('isAdmin') !== null){
-            if (isAdmin !== localStorage.getItem('isAdmin')){
-                dispatch(toggleAdmin())
-            }
-        }
-    },[])
-    useEffect(()=>{
-        localStorage.setItem('isAdmin',JSON.stringify(isAdmin))
-    },[isAdmin])
-    const clickHandler=()=>{
-        dispatch(toggleAdmin())
-    }
-    const handleAddApply=()=>{
-        dispatch(addApply(newApply))
-        console.log(newApply)
-    }
     return (
-        <div className={classes.page__content}>
-            <div className={classes.page_btn__container}>
-                <div>
-                    <Button onClick={clickHandler} view="normal" size="l">{isAdmin ? 'Войти в режим пользователя' : 'Войти в режим редактирования'}</Button>
-                    {isAdmin && <Button onClick={handleAddApply}>Добавить заявку</Button>}
-                    <AppliesList/>
-                </div>
-            </div>
-
+        <div className={classes.page}>
+            <img src={logo} alt="logo"/>
+            <Button><Link to={"/applies"}>Войти</Link></Button>
         </div>
     );
 };
