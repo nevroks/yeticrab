da5ec6f71432ae2e386175ea107ccc9ff424fee9
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-27 01:00:25 +0500

    added 2 reducers to delete and to change apply

--- src/store/applies/appliesSlice.ts
+++ src/store/applies/appliesSlice.ts
@@ -10,11 +10,37 @@ export const appliesSlice = createSlice({
     reducers: {
         addApply:(state,{payload}:PayloadAction<IApply>)=>{
             state.push(payload)
+        },
+        changeApply:(state,{payload}:PayloadAction<IApply>)=>{
+            const index=Number(payload.id)
+            const apply=state.find(applies=>applies.id===index)
+            if (apply){
+                apply.date=payload.date
+                apply.deliver=payload.deliver
+                apply.company=payload.company
+                apply.commentary=payload.commentary
+                apply.phoneNumber=payload.phoneNumber
+                apply.status=payload.status
+            }
+        },
+        deleteApply:(state,{payload}:PayloadAction<number>)=>{
+            const index = payload-1
+
+            if (index===state.length-1){
+                state.splice(-1,1)
+                return
+            }
+            state.splice(index, 1);
+            // Переопределяем айдишники
+            const changesIndex =state.indexOf(state[index])
+            for (let i=changesIndex;i<state.length;i++){
+                state[i].id=state[i].id-1
+            }
         }
     }
 })
 
 // Action creators are generated for each case reducer function
-export const {addApply} = appliesSlice.actions
+export const {addApply,changeApply,deleteApply} = appliesSlice.actions
 
 export default appliesSlice.reducer
