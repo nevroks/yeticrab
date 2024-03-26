aa4821d4a3d87052ee488041b71c434ad106a3e5
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-27 01:02:42 +0500

    added apply page

---
+++ src/pages/applypage/ApplyPage.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Link, useNavigate, useParams} from "react-router-dom";
+import {useAppSelector} from "../../hooks/reduxHooks.tsx";
+import {Button, Icon, TextInput} from "@gravity-ui/uikit";
+import {ArrowLeft} from '@gravity-ui/icons';
+import classes from "./style.module.css";
+const ApplyPage = () => {
+    const {id}=useParams()
+    const navigate=useNavigate()
+    console.log(id)
+    const apply=useAppSelector(state => state.applies[Number(id)-1])
+    return (
+        <div className={classes.page}>
+            <Button onClick={()=>navigate(-1)}><Icon data={ArrowLeft} size={18} />Назад</Button>
+            <div className={classes.apply__block}>
+                <p>ID:{apply.id}</p>
+            </div>
+            <div className={classes.apply__block}>
+                <p>Дата и время:{apply.date}</p>
+            </div>
+            <div className={classes.apply__block}>
+                <p>Название компании клиента:{apply.company}</p>
+            </div>
+            <div className={classes.apply__block}>
+                <p>ФИО перевозчика:{apply.deliver}</p>
+            </div>
+            <div className={classes.apply__block}>
+                <p>Номер перевозчика:{apply.phoneNumber}</p>
+            </div>
+            <div className={classes.apply__block}>
+                <p>Коментарий:{apply.commentary}</p>
+            </div>
+            <div className={classes.apply__block}>
+                <p>Статус:{apply.status}</p>
+            </div>
+            <div className={classes.apply__block}>
+                <p><a href="https://ati.su/firms/12345/info">ATI код:{apply.ati}(ссылка)</a></p>
+            </div>
+        </div>
+    );
+};
+
+export default ApplyPage;
