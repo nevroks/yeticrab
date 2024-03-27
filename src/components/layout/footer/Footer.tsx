964568f70c3f2d43a5d609d722a32626f02655ad
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-27 15:34:12 +0500

    filled and styled footer and header

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import classes from "./style.module.css";
 
 const Footer = () => {
     return (
         <footer className={classes.footer}>
-
+            <h2>@Условно powered by NEVROKS</h2>
         </footer>
     );
 };
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import classes from "./style.module.css";
-
+import logo from "./../../../assets/logo.png"
 const Header = () => {
     return (
             <header className={classes.header}>
-                <img className={classes.header__logo} src="" alt=""/>
-                <div></div>
+                <img className={classes.header__logo} src={logo} alt=""/>
+                <div className={classes.header__links}>
+                    <p>Здесь могли бы быть ваши ссылки</p>
+                    <p>Здесь могли бы быть ваши ссылки</p>
+                </div>
             </header>
     );
 };
