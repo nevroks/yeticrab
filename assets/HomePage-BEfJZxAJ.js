3b024bc5ae40712e0b0b619afa77ced7b5a9ae06
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-27 15:36:22 +0500

    Updates

---
+++ assets/HomePage-BEfJZxAJ.js
@@ -0,0 +1,1 @@
+import{j as s,l as a,L as e}from"./index-BqXqiHPx.js";import{B as o}from"./Button-DZOAN0Qv.js";const t="_page_a8myi_1",r={page:t},n=()=>s.jsxs("div",{className:r.page,children:[s.jsx("img",{src:a,alt:"logo"}),s.jsx(o,{children:s.jsx(e,{to:"/applies",children:"Войти"})})]});export{n as default};
---
+++ assets/reduxHooks-ZWS7o8rG.js
@@ -0,0 +1,1 @@
+import{i as s,k as e}from"./index-BqXqiHPx.js";const t=s,a=e;export{t as a,a as u};
