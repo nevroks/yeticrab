8d0b4269410884af466aee42403503432183e779
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-27 15:34:47 +0500

    removed loader from gravity ui bc it doesnt work

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,15 @@
-import React, {Suspense} from 'react';
+import {Suspense} from 'react';
 import classes from "./style.module.css";
 import {Outlet} from "react-router-dom";
 import Header from "./header/Header.tsx";
 import Footer from "./footer/Footer.tsx";
-import {Loader} from "@gravity-ui/uikit";
 
 const Layout = () => {
     return (
         <div className={classes.layoutS}>
             <Header/>
             <div className={classes.page}>
-                <Suspense fallback={<Loader/>}>
+                <Suspense fallback={<h1>Loading...</h1>}>
                     <Outlet/>
                 </Suspense>
             </div>
