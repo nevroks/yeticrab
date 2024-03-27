1869d9e050cca50daafe2d0a558c7e7534468a0c
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-27 15:33:51 +0500

    added ts ignore

--- src/utils/compareArrays.ts
+++ src/utils/compareArrays.ts
@@ -1,5 +1,6 @@
 import {IApply} from "../types/types.tsx";
 
 export default function CompareArrays (first:IApply,second:IApply){
+    // @ts-ignore
     return Object.keys({ ...first, ...second }).every(key => first[key] === second[key]);
 }
